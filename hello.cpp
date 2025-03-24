3a9cc73257ff600171012ea59ccbc7701d66263b
almira mirza <miramirza74@gmail.com>
2025-03-24 23:28:12 +0300

    first commit

---
+++ hello.cpp
@@ -0,0 +1,5 @@
+#include <iostream>  //compile için: g++ .\dosya_name.cpp -o dosya_nicknamesi
+
+int main(){
+    std :: cout <<"Hola Mundo"<< std :: endl; //çalıştırmak için: .\dosya_nicknamesi.exe
+}
